772df8c5675e388c0d4248bab1d4f14dc3a971b8
Erfan <persionhost@gmail.com>
2022-10-13 09:36:05 +0330

    Install Google tag
    https://trello.com/c/iJflHpD3/33-add-this-code-in-all-pages

--- resources/views/layouts/app-profile.blade.php
+++ resources/views/layouts/app-profile.blade.php
@@ -470,6 +470,15 @@
         @stack('js')
     </div>
     <!-- Wrapper / End -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QDD5K0BPGB"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QDD5K0BPGB');
+    </script>
 </body>
 
 </html>
