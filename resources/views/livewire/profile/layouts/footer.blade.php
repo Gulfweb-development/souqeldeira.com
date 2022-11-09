b9fe3e64ecbe4601a083b4d7a0b86ac9e99932c0
Erfan <persionhost@gmail.com>
2022-11-09 10:35:48 +0330

    fix social media link nuil
    https://trello.com/c/feH8YgOD/55-hide-fb-and-youtube-icon-from-footer

--- resources/views/livewire/profile/layouts/footer.blade.php
+++ resources/views/livewire/profile/layouts/footer.blade.php
@@ -4,10 +4,18 @@
             <p>@lang('app.copyrights')</p>
             <span>@lang('app.app_name')</span>
             <ul class="netsocials">
-                <li><a href="{{ $setting->facebook }}" target="_blank"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
-                <li><a href="{{ $setting->twitter }}" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                <li><a href="{{ $setting->instagram }}" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="{{ $setting->youtube }}" target="_blank"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
+                @if($setting->facebook)
+                    <li><a href="{{ $setting->facebook }}" target="_blank"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                @endif
+                @if($setting->twitter)
+                    <li><a href="{{ $setting->twitter }}" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                @endif
+                @if($setting->instagram)
+                    <li><a href="{{ $setting->instagram }}" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                @endif
+                @if($setting->youtube)
+                    <li><a href="{{ $setting->youtube }}" target="_blank"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
+                @endif
             </ul>
         </div>
     </div>
